53760cd1963a4822447931138623fccf189ea0fc
Robin <robincgreenwood@gmail.com>
2025-04-01 09:53:03 +0200

    added nav selections and updated global css

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,88 @@
 import Image from "next/image";
+import * as React from "react";
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
+const components: { title: string; href: string; description: string }[] = [
+  {
+    title: "Alert Dialog",
+    href: "/docs/primitives/alert-dialog",
+    description:
+      "A modal dialog that interrupts the user with important content and expects a response.",
+  },
+  {
+    title: "Hover Card",
+    href: "/docs/primitives/hover-card",
+    description:
+      "For sighted users to preview content available behind a link.",
+  },
+  {
+    title: "Progress",
+    href: "/docs/primitives/progress",
+    description:
+      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+  },
+  {
+    title: "Scroll-area",
+    href: "/docs/primitives/scroll-area",
+    description: "Visually or semantically separates content.",
+  },
+  {
+    title: "Tabs",
+    href: "/docs/primitives/tabs",
+    description:
+      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+  },
+  {
+    title: "Tooltip",
+    href: "/docs/primitives/tooltip",
+    description:
+      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+  },
+];
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
+
 export default function Home() {
   return (
     <div className="bg-black items-center justify-items-center min-h-screen gap-16 font-[family-name:var(--font-geist-sans)]">
       <div className="sticky inset-x-0 top-0 z-30 w-full transition-all">
-        <div className="absolute inset-0 block border-b transition-all border-neutral-100 bg-white/75 backdrop-blur-lg dark:border-white/10 dark:bg-black/75"></div>
+        {/* <div className="absolute inset-0 block border-b transition-all border-neutral-100 bg-white/75 backdrop-blur-lg dark:border-white/10 dark:bg-black/75"></div> */}
         <div className="mx-auto w-full px-3 relative max-w-screen-lg lg:px-4 xl:px-0">
           <div className="flex h-14 items-center justify-between">
             <div className="grow basis-0">
@@ -15,6 +94,65 @@ export default function Home() {
                 priority
               />
             </div>
+            <NavigationMenu>
+              <NavigationMenuList>
+                <NavigationMenuItem>
+                  <NavigationMenuTrigger className="bg-transparent hover:bg-gray-800">
+                    Getting started
+                  </NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                    <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                      <li className="row-span-3">
+                        <NavigationMenuLink asChild>
+                          <a
+                            className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                            href="/"
+                          >
+                            <span className="h-6 w-6" />
+                            <div className="mb-2 mt-4 text-lg font-medium">
+                              shadcn/ui
+                            </div>
+                            <p className="text-sm leading-tight text-muted-foreground">
+                              Beautifully designed components built with Radix
+                              UI and Tailwind CSS.
+                            </p>
+                          </a>
+                        </NavigationMenuLink>
+                      </li>
+                      <ListItem href="/docs" title="Introduction">
+                        Re-usable components built using Radix UI and Tailwind
+                        CSS.
+                      </ListItem>
+                      <ListItem href="/docs/installation" title="Installation">
+                        How to install dependencies and structure your app.
+                      </ListItem>
+                      <ListItem
+                        href="/docs/primitives/typography"
+                        title="Typography"
+                      >
+                        Styles for headings, paragraphs, lists...etc
+                      </ListItem>
+                    </ul>
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                    <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                      {components.map((component) => (
+                        <ListItem
+                          key={component.title}
+                          title={component.title}
+                          href={component.href}
+                        >
+                          {component.description}
+                        </ListItem>
+                      ))}
+                    </ul>
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
             <div className="hidden grow basis-0 justify-end gap-2 lg:flex">
               <a
                 href="https://app.formbricks.com/s/cm7uhrjvq0000l703slisrvf8"
@@ -31,7 +169,7 @@ export default function Home() {
           <Image
             src="/cubic_logo.png"
             alt="Fastbreak Logo"
-            width={240}
+            width={200}
             height={200}
           />
 
@@ -41,12 +179,11 @@ export default function Home() {
             </h1>
             <div className="flex flex-col">
               <p className="text-xl text-gray-400">
-                1-click animated titles for your next ad or product launch
-                video.
+                Keep eyeballs stuck on your next ad or product launch video.
               </p>
               <p className="text-xl text-gray-400">
                 You control the message.
-                <span className="text-gray-200 ms-2">
+                <span className="text-gray-200 font-semibold ms-2">
                   We make it look f*cking great.
                 </span>
               </p>
@@ -80,15 +217,15 @@ export default function Home() {
       <div className="mx-auto w-full px-3 relative max-w-screen-lg lg:px-4 xl:px-0">
         <div className="grid grid-cols-1 items-baseline gap-x-6 gap-y-12 pb-24 pt-24 sm:grid-cols-4">
           <div className="flex flex-col items-start gap-2 text-sm">
-            <p className="text-gray-700 font-medium">@ 2025 Fastbreak</p>
+            <p className="text-gray-100 font-medium">@ 2025 Fastbreak</p>
             {/* Polyfund / / Hatch / Stash */}
-            <p className="text-gray-500">A project by Nectar Labs UG</p>
-            <p className="text-gray-500">
+            <p className="text-gray-300">A project by Nectar Labs UG</p>
+            <p className="text-gray-300">
               Our mission is to add a little spice to your announcements.
             </p>
           </div>
           <div className="flex flex-inline justify-start col-span-2 items-center gap-2 text-sm">
-            <p className="text-gray-700 font-medium">Backed by</p>
+            <p className="text-gray-400 font-medium">Backed by</p>
             <Image
               width={"12"}
               height={"12"}
@@ -100,7 +237,7 @@ export default function Home() {
           <div className="flex flex-col sm:items-end gap-2 text-sm">
             <a
               href="mailto:robincgreenwood@gmail.com"
-              className="text-gray-700 hover:text-gray-600 underline font-medium"
+              className="text-gray-200 hover:text-gray-600 underline font-medium"
             >
               Get in touch
             </a>
